28ce26dc64500d162d623d7b5a0423c84048b3df
Kaique de Miranda <kbdemiranda@hotmail.com>
2024-02-15 17:18:38 -0300

    Head estatico finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {SearchBarComponent} from "./components/search-bar/search-bar.component";
+import {BannerComponent} from "./components/banner/banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SearchBarComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
